6f6d2849847fed587bac53d57aada61d6b2536f1
lokimonoton <slafe1@yandex.com>
2018-12-16 14:17:01 +0000

    Updates

---
+++ precache-manifest.d5702091dc4ca39f9e6d5847b5123b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bismillah/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "25d43cf2d33c2561b764",
+    "url": "/bismillah/static/js/runtime~main.25d43cf2.js"
+  },
+  {
+    "revision": "c25ea2a072df88e9db2b",
+    "url": "/bismillah/static/js/main.c25ea2a0.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/bismillah/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "c25ea2a072df88e9db2b",
+    "url": "/bismillah/static/css/main.e14796b3.chunk.css"
+  },
+  {
+    "revision": "bf612c0848040c1f684b7eb322e899d6",
+    "url": "/bismillah/index.html"
+  }
+];
